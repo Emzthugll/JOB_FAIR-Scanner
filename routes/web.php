7c4da9b442aaf2ce2dcb11aee8dbe4a4f7b1b2ce
Emzthugll <marcjairus11@gmail.com>
2025-10-22 16:00:09 +0800

    added reports route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScannerController;
 use App\Http\Controllers\JobfairAttendeeController;
+use App\Http\Controllers\ReportsController;
 
 Route::get('/',[ScannerController::class, 'index'])->name('home');
+Route::get('/reports', [ReportsController::class, 'index'])->name('reports.index');
 
 Route::post('/jobfair/attendees',[JobfairAttendeeController::class,'store'])->name('jobfair.attendees.store');
 
 Route::get('/jobfair/{id}/total-scanned', [JobfairAttendeeController::class, 'totalScannedAttendees'])->name('jobfair.attendees.totalScanned');
+
