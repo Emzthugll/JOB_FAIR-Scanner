a71f7dcaa87e21d5fc689a112c8e8a348a388dcf
Emzthugll <marcjairus11@gmail.com>
2025-10-30 15:32:22 +0800

    added title

--- resources/js/pages/Statistics.tsx
+++ resources/js/pages/Statistics.tsx
@@ -2,20 +2,18 @@ import SummaryCard from '@/components/ui/SummaryCard';
 import AppLayout from '@/layouts/AppLayout';
 import { Head } from '@inertiajs/react';
 import { Calendar, User } from 'lucide-react';
-import { FC } from 'react';
 
 interface StatisticsProps {
     totalScannedToday: number;
     activityType?: string | null;
 }
 
-const Statistics: FC<StatisticsProps> = ({ totalScannedToday, activityType }) => {
+const Statistics: React.FC<StatisticsProps> = ({ totalScannedToday, activityType }) => {
     return (
         <AppLayout>
             <Head title="Statistics" />
-
             <div className="mb-6 grid gap-4 md:grid-cols-3">
-                <SummaryCard title="Total Scanned" value={totalScannedToday} icon={User} />
+                <SummaryCard title="Total Scanned Today" value={totalScannedToday} icon={User} />
                 <SummaryCard title="Event Type" value={activityType || 'N/A'} icon={Calendar} />
             </div>
         </AppLayout>
